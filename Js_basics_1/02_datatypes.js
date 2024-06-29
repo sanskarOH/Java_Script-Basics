394c0929a2b1805be794fc9033e5ed31de7ba391
sanskarOH <sanskaryeeha@gmail.com>
2024-06-29 14:39:31 +0530

    introduced datatypes

---
+++ Js_basics_1/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //trea the code in strict mode or in newer version of js
+
+alert("Hello World!")//won;t work in node js it is in a documnet windows or browser
+
+//the most important thing in coding is readability of code
+//use as less as possible semi-colons
+
+console.log(3
+    +3//it will work but it is not a good practice and it is not readable
+)
+
+//original docs of js are at tc39.es
+//you can also refer to mdn docs link https://developer.mozilla.org/en-US/docs/Web/JavaScript
