8d76ef74165dfed5f46f237938d20c5d4e89ac4a
sanskarOH <sanskaryeeha@gmail.com>
2024-06-29 22:45:21 +0530

    added datatype conversion operations

--- Js_basics_1/02_datatypes.js
+++ Js_basics_1/02_datatypes.js
@@ -11,3 +11,24 @@ console.log(3
 
 //original docs of js are at tc39.es
 //you can also refer to mdn docs link https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//there are 7 datatypes in js
+//primitive datatypes
+//1. Number
+let number = 3; //range is from -2^53 to 2^53 and bigInt is used for bigger numbers
+//2. String
+let string = "Hello World";
+//3. Boolean
+let boolean = true;
+//4. Undefined
+let undefinedVar = undefined;
+//5. Null
+let nullVar = null;//standalone value is object type
+//6. Symbol
+let symbol = Symbol("symbol");//to create unique values
+//user defined datatype
+//7. Object
+let object = {
+    name: "Sanskar",
+    age: 20
+}
