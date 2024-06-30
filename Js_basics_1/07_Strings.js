4ff9c0cd3150dc7f969d7691d316f618fedbb296
sanskarOH <sanskaryeeha@gmail.com>
2024-06-30 16:07:20 +0530

    learn more about strings in js

---
+++ Js_basics_1/07_Strings.js
@@ -0,0 +1,7 @@
+//All about Strings in JavaScript
+
+const name = 'John Doe';
+const repoCount =  50;
+
+//Concatenation
+console.log('My name is ' + name + ' and I have ' + repoCount + ' repositories');
