e1d3d3a0cc1fa3fe951803884b9826e3e6cef8ca
sanskarOH <sanskaryeeha@gmail.com>
2024-06-30 16:10:37 +0530

    learn more about strings in js

--- Js_basics_1/07_Strings.js
+++ Js_basics_1/07_Strings.js
@@ -5,3 +5,10 @@ const repoCount =  50;
 
 //Concatenation
 console.log('My name is ' + name + ' and I have ' + repoCount + ' repositories');
+//using template literals
+console.log(`My name is ${name} and I have ${repoCount} repositories`);
+
+//Stering declaration
+const str = new String('Hello');
+//String is an object
+console.log(str[0]);
