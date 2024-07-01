d5cab70ed6da9b14987c15e5a26cc00b7994511a
sanskarOH <sanskaryeeha@gmail.com>
2024-07-02 00:55:04 +0530

    added maths

--- Js_basics_1/08_numberAndMaths.js
+++ Js_basics_1/08_numberAndMaths.js
@@ -22,3 +22,21 @@ console.log(number.toPrecision(2));//1.0e+2
 
 //to Local String method
 console.log(number.toLocaleString('en-IN'));//100.123456
+
+
+//++++++++++++++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++++++++
+
+//Math object
+//Math object is used to perform mathematical operations
+console.log(Math.PI);//3.141592653589793
+console.log(Math.abs(-100));//100
+console.log(Math.ceil(100.1));//101
+console.log(Math.floor(100.9));//100
+console.log(Math.round(100.5));//101
+console.log(Math.round(100.1));//100
+console.log(Math.max(100,200,300));//300
+console.log(Math.min(100,200,300));//100
+console.log(Math.pow(2,3));//8
+console.log(Math.sqrt(16));//4
+console.log(Math.random());//0.123456789
+console.log(Math.floor(Math.random()*10));//0-9
