f52ac53edb409de8118d20fa80fd690a1d3bcc57
sanskarOH <sanskaryeeha@gmail.com>
2024-07-02 01:21:20 +0530

    added dates in javascript

---
+++ Js_basics_2/array.js
@@ -0,0 +1,15 @@
+//array
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//printing the array
+console.log(myArr)
+//mdn link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+//elements can be of different types
+const My2 = ["hello",1 , 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(My2);
+//typeof Array
+console.log(typeof myArr);//object
+//in Javascript arrays are resizable
+
+
+//Accessing elements of an array
+console.log(myArr[0]);//1
