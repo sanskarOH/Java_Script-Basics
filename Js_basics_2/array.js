0086b2dd1b3bff61be5da6f8b79912945f91cf23
sanskarOH <sanskaryeeha@gmail.com>
2024-07-02 15:32:39 +0530

    added arrays

--- Js_basics_2/array.js
+++ Js_basics_2/array.js
@@ -24,3 +24,39 @@ test2[2] = 100;
 console.log(test1);//[1, 2, 100, 4, 5]
 //hence the arrays share same reference point
 //because they are stored in heap memory
+
+//another way to declare an array
+const test3 = new Array(1, 2, 3, 4, 5);
+console.log(test3[2]);//3
+
+//Array methods
+//push method
+test1.push(6);
+console.log(test1);//[1, 2, 100, 4, 5, 6]
+//tyepof Array
+console.log(typeof test1);//object
+const newA = test1.join();
+console.log(newA);//1,2,100,4,5,6
+//got converted to string
+
+//slice method
+const newB = test1.slice(1, 3);
+console.log(newB);//[2, 100]
+//3 is not included
+//splice method
+const newC = test1.splice(1, 3);
+console.log(newC);//[2, 100, 4]
+//difference between slice and splice
+//slice does not change the original array
+//splice changes the original array
+
+const na = [1, 2, 3, 4, 5];
+const nb = na.slice(1, 3);
+console.log(nb);//[2, 3]
+console.log(na);//[1, 2, 3, 4, 5]
+//na remains same
+//but in splice
+const nc = na.splice(1, 3);
+console.log(nc);//[2, 3, 4]
+console.log(na);//[1, 5]
+//na changes it removes the elements from the array
