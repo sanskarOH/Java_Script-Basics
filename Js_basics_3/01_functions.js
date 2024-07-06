c9511b045b9cf52844528453e6d77a3b1b14513a
sanskarOH <sanskaryeeha@gmail.com>
2024-07-06 12:53:21 +0530

    Added functions

--- Js_basics_3/01_functions.js
+++ Js_basics_3/01_functions.js
@@ -40,16 +40,19 @@ function add(num1 , num2 ){
 // const result = add(4,5)
 // console.log(result)
 
-
-function loginUser(username){
+//if I put here username = "sam" then it will be set as default values
+//code will atleast get sam as a values if no input is given
+function loginUser(username = "sam"){
     //checking for undefined condition
-    if(username === undefined){
-        console.log("Please enter a usernames")
+    //empty string or undefined is considerd as false
+    // if(username === undefined){
+    if(!username){
+        console.log("Please enter a username")
         return
     }
     return `Hello ${username} , just logged-in.`
 }
 //loginuser("yadav")-will returnn nothing because value is returned but not logged or used anywhere
 //not providing any value will give undefined but not null
-const res = loginUser("hitesh yadav");
+const res = loginUser("Huuhs");
 console.log(res);
