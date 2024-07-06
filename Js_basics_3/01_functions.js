ac3711a51033de018d495c9750f970e67fdf104e
sanskarOH <sanskaryeeha@gmail.com>
2024-07-06 12:49:18 +0530

    Added functions

--- Js_basics_3/01_functions.js
+++ Js_basics_3/01_functions.js
@@ -18,10 +18,38 @@
 
 function add(num1 , num2 ){
     //need not to give datatype of numberss
-    console.log(num1+num2)
+    // console.log(num1+num2)
+    return (num1+num2)//tp return the values
+    //after return no code will ever be executed
+    // console.log("hello");<unreachable code detected
 
 }
-add(3,5.6)
-add(4,"5")
-add(3,"abc")
-add(3,null)
+// add(3,5.6)
+// add(4,"5")
+// add(3,"abc")
+// add(3,null)
+
+//Difference between parameters and arguments
+//Parameters are taken at the definition fo function
+// function name(parameter1 , parameter2){}
+//Arguments are passed when calling function
+//name(argument1,argument2)
+
+//We can also store functions in a variable
+
+// const result = add(4,5)
+// console.log(result)
+
+
+function loginUser(username){
+    //checking for undefined condition
+    if(username === undefined){
+        console.log("Please enter a usernames")
+        return
+    }
+    return `Hello ${username} , just logged-in.`
+}
+//loginuser("yadav")-will returnn nothing because value is returned but not logged or used anywhere
+//not providing any value will give undefined but not null
+const res = loginUser("hitesh yadav");
+console.log(res);
