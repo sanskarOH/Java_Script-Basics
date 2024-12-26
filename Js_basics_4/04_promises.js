626e6d6e73a2fa4831ca84d5266f3099563a1d4a
sanskar <sanskaryeeha@gmail.com>
2024-12-26 10:10:37 +0530

    feat:added promises

---
+++ Js_basics_4/04_promises.js
@@ -0,0 +1,8 @@
+//Promises is for "eventual" completionn of task.
+//It is an object in JS 
+//Solution to Callnback Hell
+
+//It has two responses only 
+//1. Resolve
+//2. Reject
+//It is used for async operations
