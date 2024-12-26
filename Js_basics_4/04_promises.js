eeedad386c40533a299bc30e092db4ec095173ac
sanskar <sanskaryeeha@gmail.com>
2024-12-26 23:34:57 +0530

    feat:added promises

--- Js_basics_4/04_promises.js
+++ Js_basics_4/04_promises.js
@@ -1,8 +1,56 @@
 //Promises is for "eventual" completionn of task.
-//It is an object in JS 
+//It is an object in JS
 //Solution to Callnback Hell
 
-//It has two responses only 
+//It has two responses only
 //1. Resolve
 //2. Reject
 //It is used for async operations
+
+//There are three states of promise
+//1. Pending
+//2. Fulfilled
+//3. Rejected
+
+//Lets say we order a pizza
+//1. Pending - We order a pizza and it is in the process of making
+//2. Fulfilled - Pizza is ready and delivered
+//3. Rejected - Pizza is not available
+
+// let promise = new Promise((resolve, reject) => {
+//   console.log("Promise Started");
+//   //resolve("Success");
+//   //   reject("Error");
+// });
+
+//When we ask an api to get data, it returns a promise to us
+//So we use promises to handle the data
+
+// function getData(dataID, getNextData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataID);
+//       if (getNextData) {
+//         getNextData();
+//       }
+//     }, 5000);
+//   });
+// }
+
+//We move to console and execute the function by
+//let result = getData(1);
+//we will get promise in pending state
+//because we did not resolve or reject the promise
+
+function getData(dataID, getNextData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataID);
+      resolve("Data Loaded");
+      if (getNextData) {
+        getNextData();
+      }
+    }, 5000);
+  });
+}
+//now when we print prommise it will be in fulfilled state
